84a49e6ad405e921c6eb7e183ad13043eed88169
Bartosz Koza <kozabartosz01@gmail.com>
2025-12-05 23:18:07 +0100

    added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,39 @@
-import React from "react";
-import Tasks from "./features/tasks/Tasks";
-export const App = () => <Tasks/>
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import {
+  HashRouter,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+import AuthorPage from "./features/Author/AuthorPage";
+import { StyledNavLink } from "./styled";
+
+export const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <StyledNavLink to="/zadania">
+            Zadania
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/autor">
+            O autorze
+          </StyledNavLink>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/autor">
+          <AuthorPage />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
