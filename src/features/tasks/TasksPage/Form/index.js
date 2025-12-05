59d1a793211f53209888942bedd1788eee8c5245
Bartosz Koza <kozabartosz01@gmail.com>
2025-12-05 23:25:04 +0100

    added new Input to Form

---
+++ src/features/tasks/TasksPage/Form/index.js
@@ -0,0 +1,51 @@
+import { useState, useRef } from "react";
+import { AddTaskButton, Structure } from "./styled";
+import { useDispatch } from "react-redux";
+import { addTask } from "../../tasksSlice";
+import { nanoid } from "@reduxjs/toolkit";
+import Input from "../../Input";
+
+const Form = () => {
+  const [newTaskContent, setNewTaskContent] = useState("");
+
+
+  const dispatch = useDispatch();
+  
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(
+      addTask({
+        content: newTaskContent.trim(),
+        done: false,
+        id: nanoid(),
+      })
+    );
+
+    setNewTaskContent("");
+  };
+
+  
+
+  const inputRef = useRef();
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
+  return (
+    <Structure onSubmit={onFormSubmit}>
+      <Input
+        ref={inputRef}
+        value={newTaskContent}
+        type="text"
+        placeholder="Co jest do zrobienia?"
+        autoFocus
+        onChange={({ target }) => setNewTaskContent(target.value)}
+        required
+      />
+      <AddTaskButton onClick={focusInput}>Dodaj zadanie</AddTaskButton>
+    </Structure>
+  );
+};
+export default Form;
