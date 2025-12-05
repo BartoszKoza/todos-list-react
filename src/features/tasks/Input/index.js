d963ecc3f2b59ee272f04da17da0e8dc9c757f0c
Bartosz Koza <kozabartosz01@gmail.com>
2025-12-05 23:21:56 +0100

    added TaskPage

---
+++ src/features/tasks/Input/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default styled.input`
+  flex-basis: 170px;
+  max-width: 1000px;
+  flex-grow: 1;
+  height: 40px;
+
+  @media (max-width: 797px) {
+    margin-bottom: 10px;
+    width: 100%;
+    height: 10px;
+    flex-basis: 40px;
+  }
+`;
