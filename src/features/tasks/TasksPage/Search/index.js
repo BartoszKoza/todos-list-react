6b248c3f67b7cb33300f89c6caa39285f4869455
Bartosz Koza <kozabartosz01@gmail.com>
2025-12-06 02:24:25 +0100

    code modified

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -5,32 +5,27 @@ import {
 import Input from "../../Input";
 import { Wrapper } from "./styled";
 import searchQueryParamName from "../searchQueryParamName";
-
+import { useQueryParameter } from "../useQueryParameter";
+import { useReplaceQueryParameter } from "../useReplaceQueryParameter";
 
 export default () => {
-  const location = useLocation();
-  const history = useHistory();
-  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+  const query = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
 
-  const onInputChange = ({ target }) => {
-    const searchParams = new URLSearchParams(location.search);
-
-    if (target.value.trim() === "") {
-      searchParams.delete(searchQueryParamName);
-    } else {
-      searchParams.set(searchQueryParamName, target.value);
-    }
-
-    history.push(`${location.pathname}?${searchParams.toString()}`);
+  const onInputChange = ({target}) => {
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
   };
 
   return (
     <Wrapper>
-        <Input
-      placeholder="Filtruj zadania"
-      value={query || ""}
-      onChange={onInputChange}
-    />
+      <Input
+        placeholder="Filtruj zadania"
+        value={query || ""}
+        onChange={onInputChange}
+      />
     </Wrapper>
   );
 };
